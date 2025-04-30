9f4c0a5919d66e14da30b9015b395a2f75beb104
hvaldezrrc <hvaldez@rrc.ca>
2025-04-30 16:04:23 -0500

    Fixed testing issues

--- test/controllers/addresses_controller_test.rb
+++ test/controllers/addresses_controller_test.rb
@@ -1,13 +1,22 @@
 require "test_helper"
 
 class AddressesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+    @address = addresses(:one)
+  end
+
   test "should get edit" do
-    get addresses_edit_url
+    get edit_address_url(@address)
     assert_response :success
   end
 
   test "should get update" do
-    get addresses_update_url
-    assert_response :success
+    patch address_url(@address), params: { address: { street: "New Street", city: "New City", postal_code: "12345" } }
+
+    assert_redirected_to dashboard_url
   end
 end
